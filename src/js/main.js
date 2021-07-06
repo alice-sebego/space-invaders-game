4f0867204dbf6cfd18c723aff01fde87450cb1b8
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-07 00:53:48 +0200

    Add webpack on this project

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,23 @@
+import _ from "lodash";
 import Game from "./game.js";
 
 const $buttons = document.querySelectorAll(".nav");
-const level = [500, 250, 150];
 const $container = document.querySelector(".grid");
 const $score = document.querySelector("h3");
 let invaderId;
+const levels = [500, 250, 150];
 
 // Set more accessibility on buttons of nav
 $buttons.forEach(button => button.setAttribute("tabindex", "0"));
 
-for(let i = 0; i < level.length; i ++){
+for(let i = 0; i < levels.length; i ++){
 
     $buttons[i].addEventListener("click", () =>{
         
         $container.innerHTML = "";
         $score.innerHTML = "Score : --";
         // Build and lauch game
-        const app = new Game($container, $score, invaderId, level[i]);
+        const app = new Game($container, $score, invaderId, levels[i]);
         app.buildGridAndInvadersAndDefender();
         app.intervalInvaderId();
         // Listening user's actions
